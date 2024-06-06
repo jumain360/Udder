9096529db215041182a7813fcbcc4b25b03690a6
JMatt <juliadagiomusic@gmail.com>
2024-06-06 15:25:43 -0500

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Julián Mateo on 6/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Julián Mateo on 6/06/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
